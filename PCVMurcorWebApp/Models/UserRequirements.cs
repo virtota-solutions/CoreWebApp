8a31cebfcc146e93ccf95aa980a38adf15c4b064
cjhorton19 <30810541+cjhorton19@users.noreply.github.com>
2019-04-15 15:40:51 -0700

    Changed the requirements to include aspfor

--- PCVMurcorWebApp/Models/UserRequirements.cs
+++ PCVMurcorWebApp/Models/UserRequirements.cs
@@ -10,12 +10,16 @@ namespace PCVMurcorWebApp.Models
 
         public int FloorNum { get; set; }
         public int CubeNum { get; set; }
+        public int NewFloorNum { get; set; }
+        public int NewCubeNum { get; set; }
         public bool DidNum { get; set; }
         public bool DualMon { get; set; }
         public bool RemoAcc { get; set; }
         public bool RemoLap { get; set; }
         public bool Mobile { get; set; }
+        public bool CopyProfile { get; set; }
         public string UserStatusChange { get; set; }
         public string UserNameChange { get; set; }
+        public string LOA { get; set; }
     }
 }
